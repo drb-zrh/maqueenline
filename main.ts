ba2485de246ad099d602bca04eff0b3877bd625d
drb-zrh <94165285+drb-zrh@users.noreply.github.com>
2021-11-12 09:04:48 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let count = 0
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    }
+    if (input.compassHeading() == 0) {
+        count += 1
+        basic.showNumber(count)
+    }
+})
+basic.forever(function () {
+	
+})
